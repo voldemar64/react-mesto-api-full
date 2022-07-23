febd3b8342cd388957d141195bbdfc4bcc93723b
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-07-24 02:17:10 +0300

    some changes

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -105,6 +105,7 @@ class Api {
 const api = new Api(
   'https://mesto.vova.nomoredomains.xyz',
   {
+    authorization: '8ecd362d-8687-40cb-8171-87d5d8b4e71d',
     'Content-Type': 'application/json'
   }
 );
