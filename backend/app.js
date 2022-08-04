d411c2c0ecf10482673c374c616390ef2b14a7ce
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-08-04 13:11:16 +0300

    edited: cors and IP

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://mesto.vova.nomoredomains.xyz',
+    origin: [
+      'https://mesto.vova.nomoredomains.xyz',
+      'http://mesto.vova.nomoredomains.xyz',
+    ]
+    credentials: true,
   }),
 );
 
