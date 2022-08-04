d0d365683bee83a07d52727c17e911e55d8b0389
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-08-04 14:34:38 +0300

    cors2.0

--- backend/app.js
+++ backend/app.js
@@ -18,15 +18,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: [
-      'https://mesto.vova.nomoredomains.xyz',
-      'http://mesto.vova.nomoredomains.xyz',
-      'https://api.mesto.vova.nomoredomains.xyz',
-      'http://api.mesto.vova.nomoredomains.xyz',
-      'https://api.mesto.vova.nomoredomains.xyz/users/me',
-    ],
-  }),
+  cors(),
 );
 
 app.get('/crash-test', () => {
