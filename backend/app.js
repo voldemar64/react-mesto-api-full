fef53473c7fd9c1e08168193562e7e825e7e5eb6
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-10-04 20:06:50 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const { errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const { signinValidation, signupValidation } = require('./middlewares/validation');
@@ -11,22 +10,14 @@ const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errors');
 const NotFound = require('./errors/NotFound');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [
-      'https://mesto.vova.nomoredomains.xyz',
-      'http://mesto.vova.nomoredomains.xyz',
-      'https://api.mesto.vova.nomoredomains.xyz',
-      'http://api.mesto.vova.nomoredomains.xyz',
-    ],
-  }),
-);
+app.use(cors);
 
 app.use(requestLogger);
 
---
+++ backend/middlewares/cors.js
@@ -0,0 +1,26 @@
+const allowedCors = [
+  'https://mesto.vova.nomoredomains.xyz',
+  'http://mesto.vova.nomoredomains.xyz',
+  'https://api.mesto.vova.nomoredomains.xyz',
+  'http://api.mesto.vova.nomoredomains.xyz',
+];
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+};
+
+module.exports = cors;
